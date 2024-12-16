abf98e863ada78cb651587eb74938d0f46871e4c
Leandro Favre <atiladevelop@gmail.com>
2024-12-16 19:09:36 -0300

    versions updated

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import { View, Text } from "react-native";
 
 const App = () => {
   return (
-    <View className="flex-1 justify-center items-center">
+    <View className="items-center justify-center flex-1">
       <Text className="text-3xl font-bold">
         Minimal Expo Setup + NativeWind
       </Text>
